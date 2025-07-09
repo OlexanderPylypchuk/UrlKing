1f61abd1570261820512ef4ce752d4839bfc7d68
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-07-09 17:23:49 +0300

    Models

---
+++ UrlKing.Server/Models/AboutContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace UrlKing.Server.Models
+{
+	public class AboutContent
+	{
+		public int Id { get; set; }
+		public string Content { get; set; }
+		public DateTime SavedTime { get; set; }
+		[ForeignKey("ApplicationUser")]
+		public string UserId { get; set; }
+		[ValidateNever]
+		public ApplicationUser ApplicationUser { get; set; }
+	}
+}
---
+++ UrlKing.Server/Models/ShortenedUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace UrlKing.Server.Models
+{
+	public class ShortenedUrl
+	{
+		[Key]
+		public string Code { get; set; }
+		[Required]
+		public string LongUrl { get; set; }
+		[ValidateNever]
+		public string ShortUrl { get; set; }
+		public DateTime CreatedDate { get; set; }
+		[ForeignKey("ApplicationUser")]
+		public string UserId { get; set; }
+		[ValidateNever, DisplayName("CreatedBy")]
+		public ApplicationUser ApplicationUser { get; set; }
+	}
+}
